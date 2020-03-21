48e407002a0b53d0b03d755eb7d5446e578053d9
Jan Küster <jkuester@uni-bremen.de>
2020-03-21 20:40:34 +0100

    tests/api/Routes added

---
+++ tests/api/routing/Routes.tests.js
@@ -0,0 +1,76 @@
+/* elsint-env mocha */
+import { check, Match } from 'meteor/check'
+import { Routes } from '../../../imports/api/routing/Routes'
+import { i18n } from '../../../imports/api/i18n/I18n'
+import { assert } from 'chai'
+import sinon from 'sinon'
+
+const allRoutes = Object.keys(Routes)
+
+describe('Routes', function () {
+
+  beforeEach(function () {
+    sinon.stub(i18n, 'get').callsFake(x => x)
+  })
+
+  afterEach(function () {
+    i18n.get.restore()
+  })
+
+  describe('properties', function () {
+
+    it('path', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        const path = route.path()
+        check(path, String)
+      })
+    })
+
+    it('label', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        check(route.label, String)
+      })
+    })
+
+    it('triggersEnter', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        const triggersEnter = route.triggersEnter && route.triggersEnter()
+        check(triggersEnter, [Function])
+      })
+    })
+
+    it('load', function () {
+      const failed = []
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        const promise = route.load()
+        promise.catch(() => {})
+        assert.equal(promise.constructor.name, 'Promise')
+      })
+    })
+
+    it('target', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        check(route.target, Match.Maybe(String))
+      })
+    })
+
+    it('template', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        check(route.template, String)
+      })
+    })
+
+    it('data', function () {
+      allRoutes.forEach(key => {
+        const route = Routes[key]
+        check(route.target, Match.Maybe(Object))
+      })
+    })
+  })
+})
