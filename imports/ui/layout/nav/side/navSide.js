d1044b1378f9d0a7480668b5b41c4f3752a6b9a4
Tushar <tushargarg495@gmail.com>
2020-04-04 18:26:56 +0200

    background-color to sidebar added

--- imports/ui/layout/nav/side/navSide.js
+++ imports/ui/layout/nav/side/navSide.js
@@ -1,4 +1,19 @@
 import { Template } from 'meteor/templating'
 import './navSide.html'
+import { Routes } from '../../../../api/routing/Routes'
+import { Router } from '../../../../api/routing/Router'
 
-Template.navSide.helpers({})
+let currentElement = null
+let currentPath = null
+
+Template.navSide.helpers({
+})
+
+Template.navSide.events({
+  'click .nav-link' (e) {
+    // console.log(e.currentTarget.id)
+    // currentElement = e.currentTarget.id
+    // console.log(Router.go())
+    // currentPath = Routes.class.load()
+  }
+})
