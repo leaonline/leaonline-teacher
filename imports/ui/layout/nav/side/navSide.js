579c1ef657f734d570615fe5a49680de9206c02a
Tushar <tushargarg495@gmail.com>
2020-04-21 14:17:29 +0200

    navlinks replaced with links

--- imports/ui/layout/nav/side/navSide.js
+++ imports/ui/layout/nav/side/navSide.js
@@ -4,16 +4,20 @@ import { bbsComponentLoader } from '../../../utils/bbsComponentLoader'
 import { Session } from '../../../../api/session/Session'
 import './navSide.html'
 import './scss/navSlide.scss'
+import { Router } from '../../../../api/routing/Router'
 
 const componentsLoader = bbsComponentLoader([
   BlazeBootstrap.link.load(),
   BlazeBootstrap.button.load(),
-  BlazeBootstrap.navbar.load()
+  BlazeBootstrap.navbar.load(),
+  BlazeBootstrap.nav.load()
 ])
+
 const componentsLoaded = componentsLoader.loaded
 
 Template.navSide.helpers({
   componentsLoaded () {
+    console.log(Router.label())
     return componentsLoaded.get()
   },
   currentClass () {
