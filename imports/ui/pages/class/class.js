5a67e2b40b6fb97ac1cef3bc50a52c4970ca297b
Jan Küster <jkuester@uni-bremen.de>
2020-04-22 15:47:51 +0200

    ui pages check / set Session variables onCreated

--- imports/ui/pages/class/class.js
+++ imports/ui/pages/class/class.js
@@ -1,1 +1,21 @@
+import { Template } from 'meteor/templating'
+import { Session } from '../../../api/session/Session'
+import { classExists } from '../../utils/classExists'
 import './class.html'
+
+Template.class.onCreated(function () {
+  const instance = this
+  instance.autorun(() => {
+    const data = Template.currentData()
+    const { classId } = data.params
+    const currentClass = Session.currentClass()
+
+    if (classExists() && currentClass !== classId) {
+      Session.currentClass(classId)
+    }
+
+    if (Session.currentParticipant()) {
+      Session.currentParticipant(null)
+    }
+  })
+})
