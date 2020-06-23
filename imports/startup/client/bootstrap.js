cba4fe49b1b10c63474becde3bb8733d9e4d56ce
Jan Küster <jkuester@uni-bremen.de>
2020-06-23 15:08:42 +0200

    fix startup missing scss / theme issue

--- imports/startup/client/bootstrap.js
+++ imports/startup/client/bootstrap.js
@@ -2,4 +2,4 @@ import 'bootstrap'
 import popper from 'popper.js'
 import './scss/custom.scss'
 
-global.Popper = popper // fixes some issues with Popper and Meteor
+global.Popper = global.Popper || popper // fixes some issues with Popper and Meteor
