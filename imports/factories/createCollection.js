3c9ada8509d070df2b92e19394971f3b36487db0
Jan Küster <jkuester@uni-bremen.de>
2020-11-26 12:11:55 +0100

    removed unused factories

--- imports/factories/createCollection.js
+++
@@ -1,4 +0,0 @@
-import { getCreateCollection } from 'meteor/leaonline:factories/collection/createCollection'
-import { Schema } from '../api/schema/Schema'
-
-export const createCollection = getCreateCollection(Schema.create)
--- imports/factories/createMethods.js
+++
@@ -1,4 +0,0 @@
-import { getCreateMethods } from 'meteor/leaonline:factories/method/createMethods'
-import { Schema } from '../api/schema/Schema'
-
-export const createMethods = getCreateMethods(Schema.create)
