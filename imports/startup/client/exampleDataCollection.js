28e12f7b2324f87f71eb5f56b9c5705822c328bd
Tushar <tushargarg495@gmail.com>
2021-03-09 11:16:13 +0100

    dummy data for competency created

--- imports/startup/client/exampleDataCollection.js
+++ imports/startup/client/exampleDataCollection.js
@@ -1,10 +1,13 @@
 import { Meteor } from 'meteor/meteor'
 import courseInfo from '../../../resources/exampleData/coursesInfo'
 import competencyCategories from '../../../resources/exampleData/competencyCategories'
+import competency from '../../../resources/exampleData/competency'
 import { MyCourses } from '../../api/collections/MyCourses'
 import { CompetencyCategories } from '../../api/collections/competencyCategories'
+import { Competency } from '../../api/collections/competency'
 
 Meteor.startup(() => {
   courseInfo.forEach(document => MyCourses.api.insert(document))
   competencyCategories.forEach(document => CompetencyCategories.api.insert(document))
+  competency.forEach(document => Competency.api.insert(document))
 })
