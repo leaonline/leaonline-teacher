321cd731d88c136943dca03f604bf05837c26eb3
Tushar <tushargarg495@gmail.com>
2021-03-09 11:09:43 +0100

    card flipping feature added to competency cards

--- imports/ui/pages/user/user.js
+++ imports/ui/pages/user/user.js
@@ -3,10 +3,12 @@ import { Session } from '../../../api/session/Session'
 import { classExists } from '../../utils/classExists'
 import { userExists } from '../../utils/userExists'
 import { CompetencyCategories } from '../../../api/collections/competencyCategories'
+import { Competency } from '../../../api/collections/competency'
 // import { Schema } from '../../../api/schema/Schema'
-import './user.html'
 import { bbsComponentLoader } from '../../utils/bbsComponentLoader'
 import { BlazeBootstrap } from '../../../api/blazebootstrap/BlazeBootstrap'
+import './user.html'
+import './scss/user.scss'
 
 Template.user.onCreated(function () {
   const instance = this
@@ -42,14 +44,41 @@ const componentsLoaded = componentsLoader.loaded
 // const competencyCategoriesSchema = Schema.create(CompetencyCategories.schema)
 // const collection = CompetencyCategories.collection()
 
+const cursor = CompetencyCategories.collection().find({}, { fields: { title: 0 } }).fetch()
+console.log(cursor)
+
+const competency = Competency.collection().find().fetch()
+
+cursor.forEach(function (cursorDoc) {
+  competency.forEach(function (document) {
+    Competency.collection().update(document._id, { $set: { competencyCategory: cursorDoc._id } })
+  })
+})
+
+console.log(Competency.collection().find().fetch())
+
 Template.user.helpers({
   componentsLoaded () {
     return componentsLoaded.get()
   },
   competencyCategories () {
     const cursor = CompetencyCategories.collection().find()
-    console.log(cursor.fetch())
+    // console.log(cursor.fetch())
     if (cursor.count() === 0) return null
     return cursor
+  },
+  competencies () {
+    const cursor = Competency.collection().find()
+    // console.log(cursor.fetch())
+    if (cursor.count() === 0) return null
+    return cursor
+  }
+})
+
+Template.user.events({
+  'click .info-icon' (event, templateInstance) {
+    event.preventDefault()
+    event.stopImmediatePropagation()
+    templateInstance.$(event.currentTarget).closest('.flip-card-inner').toggleClass('flip')
   }
 })
