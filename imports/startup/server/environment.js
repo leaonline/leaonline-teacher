e7a44af6365b12d107db939575bc24be44a1b1d5
Jan Küster <jkuester@uni-bremen.de>
2022-01-12 14:09:58 +0100

    fix environment restriction to run.sh only for development

--- imports/startup/server/environment.js
+++ imports/startup/server/environment.js
@@ -1,6 +1,6 @@
 const { RUN_FLAG } = process.env
 
-if (!RUN_FLAG) {
+if (Meteor.isDevelopment && !RUN_FLAG) {
   console.error('-------------------------------------------------------------------------------------------------')
   console.error()
   console.error('[FATAL]: startup cancelled. Please run the app using the [run.sh] script from the project folder.')
